b7c364132551e671db6f382ca51c2f8fa6dd1325
Don Ayers <dayers01@seattlecentral.edu>
2014-05-13 20:14:08 +0000

    fixed index to allow alive filter

--- assignment-4/index.php
+++ assignment-4/index.php
@@ -6,10 +6,6 @@ $mysql = new mysqli("localhost", "dayers01", $mysql_pass, "dayers01");
 
 include("filter.php");
 
-$query->execute();
-
-$result = $query->get_result();
-
 include("resultspage.php");
 
 ?>
